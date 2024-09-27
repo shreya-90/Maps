767de57364f2ade1607fda286c51a928f853b526
Shreya Pallan <shreyapallan@Shreyas-MBP.lan>
2024-09-27 16:27:37 +0530

    Adding location model and Location service data

---
+++ SwiftfulMapApp/SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Shreya Pallan on 27/09/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
--- SwiftfulMapApp/SwiftfulMapApp/SwiftfulMapApp.swift
+++ SwiftfulMapApp/SwiftfulMapApp/SwiftfulMapApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct SwiftfulMapAppApp: App {
+struct SwiftfulMapApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
