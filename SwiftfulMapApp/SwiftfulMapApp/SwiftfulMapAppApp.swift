39a0d5a12d1946b51cd1085a3f627b902b94fb54
Shreya Pallan <shreyapallan@Shreyas-MBP.lan>
2024-09-27 11:22:14 +0530

    Initial project setup

---
+++ SwiftfulMapApp/SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Shreya Pallan on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
